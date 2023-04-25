33d1c55c71e523ee63b4a4d08ea38b7087b0c617
Martin Crabtree <martin.crabtree@gmail.com>
2023-04-24 21:35:04 -0400

    pulled and updated

--- tristram/src/components/header/Header.tsx
+++ tristram/src/components/header/Header.tsx
@@ -2,15 +2,15 @@ import React, { ReactNode } from "react";
 
 type HeadingProps = {
   title: string;
-  children: ReactNode;
+  children?: ReactNode;
 };
 
 const Header = ({ title, children }: HeadingProps) => {
   return (
-    <div>
+    <>
       <h1>{title}</h1>
       <p>{children}</p>
-    </div>
+    </>
   );
 };
 
