244bf4b50f4bd98c38abbb64cb4d510508808e17
Martin Crabtree <martin.crabtree@gmail.com>
2023-04-30 13:38:14 -0400

    general notes and adding styled components

--- tristram/src/App.tsx
+++ tristram/src/App.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import Header from "./components/header";
 import "./App.css";
 
+// todo header, footer, hero, body
+// body: skills & expertise, projects, experience
+
 function App() {
   return (
     <div className="App">
