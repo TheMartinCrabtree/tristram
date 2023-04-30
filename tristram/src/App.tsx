e7aba6a5f7e36b7dd5e97c6393447eb7925e376f
Martin Crabtree <martin.crabtree@gmail.com>
2023-04-30 14:08:29 -0400

    setting general header layout

--- tristram/src/App.tsx
+++ tristram/src/App.tsx
@@ -1,16 +1,23 @@
 import React from "react";
 import styled from "styled-components";
 import Header from "./components/header";
-import "./App.css";
+// import "./App.css";
 
 // todo header, footer, hero, body
 // body: skills & expertise, projects, experience
 
+const TempBody = styled.div`
+  min-height: 800px;
+  background-color: black;
+  border-radius: 3px;
+  border: 2px solid darkgrey;
+`;
+
 function App() {
   return (
-    <div className="App">
+    <TempBody className="App">
       <Header title="Header Title" />
-    </div>
+    </TempBody>
   );
 }
 
--- tristram/src/components/header/Header.tsx
+++ tristram/src/components/header/Header.tsx
@@ -1,16 +1,28 @@
 import React, { ReactNode } from "react";
+import styled from "styled-components";
 
 type HeadingProps = {
   title: string;
   children?: ReactNode;
 };
 
+const HeaderWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin: 0px;
+  background-color: white;
+  opacity: 0.95;
+  color: black;
+`;
+
 const Header = ({ title, children }: HeadingProps) => {
   return (
-    <>
+    <HeaderWrapper>
       <h1>{title}</h1>
       <p>{children}</p>
-    </>
+    </HeaderWrapper>
   );
 };
 
